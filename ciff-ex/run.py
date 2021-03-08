1b0fe49a0cab1404b660ae1dbd64f920d988974e
gasplant64 <gasplant64@snu.ac.kr>
2021-03-08 11:25:15 +0900

    Deleted unnessary files

--- ciff-ex/run.py
+++
@@ -1,9 +0,0 @@
-#run.py
-
-from _example import ffi, lib
-
-@ffi.callback("int(int, int *)")
-def python_callback(how_many, values):
-    print (ffi.unpack(values, how_many))
-    return 0
-lib.python_callback = python_callback
